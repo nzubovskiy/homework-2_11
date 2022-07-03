f8eb7e80ef779ddbe9a06fba4babf539937f6853
NIKO <nzubovskiy@gmail.com>
2022-07-03 14:02:52 +0400

    Доработка

--- src/main/java/com/example/homework2_11/controllers/ItemsController.java
+++ src/main/java/com/example/homework2_11/controllers/ItemsController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/order")
+@RequestMapping("/store/order")
 public class ItemsController {
 
     private final OrderService orderService;
