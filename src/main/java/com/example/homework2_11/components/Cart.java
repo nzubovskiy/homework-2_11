c91785b467a11b22e32132747d2bba6e2032fd82
NIKO <nzubovskiy@gmail.com>
2022-07-03 10:11:13 +0400

    Homework

---
+++ src/main/java/com/example/homework2_11/components/Cart.java
@@ -0,0 +1,28 @@
+package com.example.homework2_11.components;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+@SessionScope
+public class Cart {
+
+    private final List<Integer> items;
+
+    public Cart() {
+        this.items = new ArrayList<>();
+    }
+
+    public List<Integer> addItems(List<Integer> idList) {
+        items.addAll(idList);
+        return idList;
+    }
+
+    public List<Integer> getItems() {
+        return Collections.unmodifiableList(items);
+    }
+}
--- src/main/java/com/example/homework2_11/controllers/ItemsController.java
+++ src/main/java/com/example/homework2_11/controllers/ItemsController.java
@@ -1,7 +1,9 @@
 package com.example.homework2_11.controllers;
 
+import com.example.homework2_11.services.OrderService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +19,12 @@ public class ItemsController {
     }
 
     @GetMapping("/add")
-    public List<Integer>
+    public List<Integer> addItems(@RequestParam List<Integer> id) {
+        return orderService.addItems(id);
+    }
+
+    @GetMapping("/get")
+    public List<Integer> getItems() {
+        return orderService.getItems();
+    }
 }
